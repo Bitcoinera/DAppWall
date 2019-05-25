44a0b61c6ec3aac36db62e421bfbbaa36fdba0fa
VG <31324402+VGCif@users.noreply.github.com>
2019-05-25 16:48:56 +0200

    SmartContract DAppWall

---
+++ DAppWallContract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.1;
+
+contract DappWall {
+    event addIP(
+        address indexed _from,
+        bytes32 indexed _swarmHashList,
+    );
+
+    function update(bytes32 _swarmHashList) public payable{
+        // Events are emitted using 'emit', followed by
+        // the name of the event and the arguments
+        // (if any) in parentheses. Any such invocation
+        // (even deeply nested) can be detected from
+        // the JavaScript API by filtering for 'Deposit'
+        emit addIP(msg.sender, _swarmHashList);
+    }
+}

f1a0a2dd657b8673719e3eae90c29c81d37106de
VG <31324402+VGCif@users.noreply.github.com>
2019-05-25 19:16:35 +0200

    Fixes

--- src/main.js
+++ src/main.js
@@ -5,18 +5,3 @@ form.addEventListener('submit', async (e) => {
     //e.preventDefault();
     console.log(input.value);
 })
-
-
-// Para coger el código del Smart Contract
-var upadateEvent = dappWallContract.Deposit({_swarmHashList: web3.eth.coinbase});
-upadateEvent.watch(function(err, result) {
-  if (err) {
-    console.log("Esto no sale")
-    return;
-  }
-  console.log(result.args._value)
-  // check that result.args._swarmHashList is web3.eth.coinbase then
-  // display result.args._value in the UI and call
-  // upadateEvent.stopWatching()
-})
-dappWallContract.update.sendTransaction(2, {from: web3.eth.coinbase})

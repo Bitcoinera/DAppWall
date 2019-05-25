b65e5863ec73965b2bdecf0c9e55cd43f96eb8cb
VG <31324402+VGCif@users.noreply.github.com>
2019-05-25 18:38:47 +0200

    GETfromSmartContract

---
+++ GETfromSmartContract.js
@@ -0,0 +1,42 @@
+const web3 = new Web3("https://rinkeby.infura.io/v1");
+const DappWallABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_swarmHashList",
+				"type": "bytes32"
+			}
+		],
+		"name": "update",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "_swarmHashList",
+				"type": "bytes32"
+			}
+		],
+		"name": "listIP",
+		"type": "event"
+	}
+]
+const DappWallContract = new web3.eth.Contract(DappWallABI, 0x0dcd2f752394c41875e259e00bb44fd505297caf)
+
+DappWallContract.getPastEvents('listIP', {
+    fromBlock: 4444785, 	//meter el bloque donde se despliega el contrato
+    toBlock: 'latest'
+}, (error, events) => {
+	console.log(events);
+})

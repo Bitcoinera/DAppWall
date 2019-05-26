68e323cf5a267e8a4a06dc44d7985e4e1db774a9
anagutjor <anagjordano@gmail.com>
2019-05-26 12:22:34 +0200

    add fetch to main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 let container = document.querySelectorAll('div')[0];
 let form = document.getElementById('ip-form');
-let input = document.getElementById('ip');
+let ip = document.getElementById('ip');
+let id = document.getElementById('id');
 let contractEvents;
 let swarmHashList;
 
 form.addEventListener('submit', async (e) => {
-    //e.preventDefault();
-    console.log(input.value);
+    e.preventDefault();
     let contract = window.contract;
 
     // events = SmartContractGet(contract)
@@ -18,7 +18,6 @@ form.addEventListener('submit', async (e) => {
         contractEvents = events[0].returnValues;
         swarmHashList = contractEvents['_swarmHashList'];
         swarmHashList = swarmHashList.slice(2, swarmHashList.length);
-        console.log('This is the list ' + swarmHashList);
 
         let paragraph = document.createElement('p');
         paragraph.innerHTML = 'This is your hash';
@@ -29,6 +28,39 @@ form.addEventListener('submit', async (e) => {
         hash.href = swarmHashList;
         hash.innerHTML = swarmHashList;
         container.appendChild(hash);
+
+        let formData = {
+            id: id.value,
+            ip: ip.value
+        }
+
+        formData = JSON.stringify(formData);
+        console.log(formData);
+
+        // POST to Swarm with fetch
+        fetch("http://swarm.protocol-bt.ml/bzz:/", {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            mode: "no-cors",
+            method: "POST",
+            body: formData
+          })
+          .then((res) => console.log(res))
+          .catch(res => console.log(res));
+
+        // GET from Swarm with fetch
+        fetch(`http://swarm.protocol-bt.ml/bzz:/${swarmHashList}`, {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            mode: "no-cors",
+            method: "POST",
+        })
+        .then((res) => console.log(res))
+        .catch(res => console.log(res));
 
         // contract.methods.update(swarmHashList).send({from: '0x0dcd2f752394c41875e259e00bb44fd505297caf'}, (error, transactionHash) => {
         //     console.log(transactionHash);
